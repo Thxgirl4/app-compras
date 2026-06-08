592e69692188223b6d7881f4ed1446836025da85
Thxgirl4 <anajuliasilva3304@gmail.com>
2026-06-07 23:08:38 -0300

    feat: add StatusIcon em Filter

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,7 +1,12 @@
+import React from "react";
 import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
 
 import { styles } from "./styles";
 import { FilterStatus } from "@/shared-types/FilterStatus";
+import { StatusIcon } from "../StatusIcon";
+
+
+
 
 type Props = TouchableOpacityProps & {
     status: FilterStatus
@@ -14,7 +19,8 @@ export function Filter({ status, isActive, ...rest }: Props) {
         <TouchableOpacity {...rest}
             activeOpacity={0.8}
             style={[styles.container, { opacity: isActive ? 1 : 0.5 }]}>
-
+            
+            <StatusIcon status={status}/>
             
             <Text style={styles.title}>
                 {status === FilterStatus.DONE ? "Comprado" : "Pendente"}
